96c04a9c91bda47c3f7c4c0afd9644bb58000a45
julien <jln.buisseret@gmail.com>
2014-11-27 04:31:12 +0100

    env: Add mayapy system: Add maya alias

--- lib/env/env.py
+++ lib/env/env.py
@@ -9,6 +9,8 @@ toolPack = __package__
 
 #-- Studio Var --#
 wsPath = os.sep.join(toolPath.split(os.sep)[:-2])
-if not wsPath in sys.path:
-    print "[sys] | Info | Add %s to sysPath" % wsPath
-    sys.path.insert(0, wsPath)
+qtPath = "C:/Python27/Lib/site-packages"
+for path in [wsPath, qtPath]:
+    if not path in sys.path:
+        print "[sys] | Info | Add %s to sysPath" % path
+        sys.path.insert(0, path)
