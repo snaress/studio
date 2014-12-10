a649b9987a197d7d6b3c3a77f121c73de746ab9e
julien <jln.buisseret@gmail.com>
2014-12-10 18:36:56 +0100

    camPrezUi: update property

--- tools/maya/camera/camPrez/camPrezUi.py
+++ tools/maya/camera/camPrez/camPrezUi.py
@@ -16,30 +16,24 @@ class CamPrez(QtGui.QMainWindow, camPrezUI.Ui_mwCamPrez):
         self.log.info("#-- Launching CamPrez Ui --#")
         super(CamPrez, self).__init__(parent)
         self._setupUi()
-        self.rf_ui()
-        print 'launch 1'
 
     # noinspection PyUnresolvedReferences
     def _setupUi(self):
         """ Setup main ui """
         self.setupUi(self)
-        print 'connect'
         self.bCreateCamTurn.clicked.connect(self.on_createCamTurn)
-
-    def rf_ui(self):
-        print 'toto'
 
     def on_createCamTurn(self):
         """ Command launched when 'Create Camera Turn' is clicked """
-        print 'launch 2'
-        cpCmds.createCamTurn(self.getFrontAxe('turn'), self.getDuration('turn'))
+        cpCmds.createCamTurn(self.getFrontAxe, self.getDuration)
 
-    def getFrontAxe(self, mode):
+    @property
+    def getFrontAxe(self):
         """ Get front axe from given mode
             :param mode: (str) : 'turn' or 'quadra'
             :return: (str) : Front axe """
-        print self.tabWidget.currentIndex()
-        if mode == 'turn':
+        curTab = self.tabWidget.tabText(self.tabWidget.currentIndex())
+        if curTab == 'Turn':
             if self.cbTurnX.isChecked():
                 if self.cbTurnInvert.isChecked():
                     return '-x'
@@ -53,11 +47,13 @@ class CamPrez(QtGui.QMainWindow, camPrezUI.Ui_mwCamPrez):
                     return '-z'
                 return 'z'
 
-    def getDuration(self, mode):
+    @property
+    def getDuration(self):
         """ Get duration from given mode
             :param mode: (str) : 'turn' or 'quadra'
             :return: (int) : Duration """
-        if mode == 'turn':
+        curTab = self.tabWidget.tabText(self.tabWidget.currentIndex())
+        if curTab == 'Turn':
             if self.cbInvertRotate.isChecked():
                 return int(self.sbTurnDuration.value())*-1
             return int(self.sbTurnDuration.value())
@@ -68,7 +64,6 @@ def launch():
         :return: (object) : Launched window """
     toolName = 'mwCamPrez'
     if mc.window(toolName, q=True, ex=True):
-        print "Delete CamPrez Window"
         mc.deleteUI(toolName, wnd=True)
     global window
     window = CamPrez(parent=pUi.getMayaMainWindow())
