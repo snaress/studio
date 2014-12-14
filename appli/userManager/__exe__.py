b52c08d647524f1a1adfce404b7f7bfc25f1205a
julien <jln.buisseret@gmail.com>
2014-12-14 07:18:57 +0100

    remove env setter from launcher

--- appli/userManager/__exe__.py
+++ appli/userManager/__exe__.py
@@ -1,4 +1,4 @@
-import os, sys, optparse
+import optparse
 
 
 #-- Options --#
@@ -11,17 +11,6 @@ parser.add_option('-v', '--verbose', type='choice', default='info',
 
 
 if __name__ == '__main__':
-    #-- Package Var --#
-    toolPath = os.path.normpath(os.path.dirname(__file__))
-    toolName = toolPath.split(os.sep)[-1]
-    toolPack = __package__
-
-    #-- SysPath --#
-    wsPath = os.sep.join(toolPath.split(os.sep)[:-2])
-    if not wsPath in sys.path:
-        print "[sys] | Info | Add %s to sysPath" % wsPath
-        sys.path.insert(0, wsPath)
-
     options, args = parser.parse_args()
     options = eval(str(options))
 
--- lib/qt/__exeSE__.py
+++ lib/qt/__exeSE__.py
@@ -1,22 +1,8 @@
-import os, sys
-
-
-#-- Package Var --#
-toolPath = os.path.normpath(os.path.dirname(__file__))
-toolName = toolPath.split(os.sep)[-1]
-toolPack = __package__
-
-
-#-- SysPath --#
-wsPath = os.sep.join(toolPath.split(os.sep)[:-2])
-if not wsPath in sys.path:
-    print "[sys] | Info | Add %s to sysPath" % wsPath
-    sys.path.insert(0, wsPath)
-
-
-#-- Launch --#
+import sys
 from PyQt4 import QtGui
 from lib.qt import scriptEditor
+
+
 app = QtGui.QApplication(sys.argv)
 window = scriptEditor.ScriptEditor()
 window.show()
